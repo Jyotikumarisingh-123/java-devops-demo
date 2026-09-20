7902ea418d0acd5559527d7fa3318611a3217cbe
Jyoti Kumari <123.jyotikumarisingh@gmail.com>
2026-09-20 18:07:55 +0530

    Initial Java DevOps project

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Java DevOps Application Started!");
+        System.out.println("Welcome to my Java DevOps project.");
+    }
+}
