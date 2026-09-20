c811f78e4962baccca673db5684df3a00ecdf9f9
Jyoti Kumari <123.jyotikumarisingh@gmail.com>
2026-09-20 18:43:37 +0530

    Reorganize Java project into Maven structure

--- src/main/java/com/democompany/myfirstjavaproject/App.java
+++ src/main/java/com/democompany/myfirstjavaproject/App.java
@@ -1,7 +1,9 @@
+package com.democompany.myfirstjavaproject;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Java DevOps Application Started!");
-        System.out.println("Welcome to my Java DevOps project.");
+        System.out.println("Welcome to my first Java project.");
     }
 }
---
+++ src/test/java/com/democompany/myfirstjavaproject/AppTest.java
@@ -0,0 +1,8 @@
+package com.democompany.myfirstjavaproject;
+
+public class AppTest {
+
+    public static void main(String[] args) {
+        System.out.println("AppTest started successfully.");
+    }
+}
